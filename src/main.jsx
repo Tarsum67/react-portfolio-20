c7adf50c1d7dde3e9f9e99fb3e83254e5370352c
Tarsum67 <taraum@icloud.com>
2023-12-02 16:53:35 -0600

    added code to app main and contact page and changed packages

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import AboutMePage from './pages/AboutMePage';
+import ContactMePage from './pages/ContactMePage';
+import PortfolioPage from './pages/PortfolioPage';
+import Resumepage from './pages/ResumePage';
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMePage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactMePage />,
+      },
+      {
+        path: 'resume',
+        element: <Resumepage />,
+      },
+    ],
+  },
+]);
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
