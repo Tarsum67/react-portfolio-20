0910706fd8216fa22584c88d29c7c32d1dd7b6d3
Tarsum67 <taraum@icloud.com>
2023-12-26 19:04:58 -0600

    updated

--- src/components/UI/ProjectCard.jsx
+++ src/components/UI/ProjectCard.jsx
@@ -5,7 +5,7 @@ export default function ProjectCard(props) {
     <div key={index} className='project-container'>
       <a href={project.url} target="_blank" className="rel">
         <img src={project.img} alt="" className="project-img "/>
-        <div className="project-info 100w 100h justify-content-center align-items-center my-3">
+        <div className="project-info ">
           <a className="p-2" href={project.url} target="_blank">{project.name}</a>
           <a className="p-2" href={project.github} target="_blank"><img className="logo-sm bg-black" src={github} alt="" /></a>
         </div>
@@ -14,26 +14,7 @@ export default function ProjectCard(props) {
   ));
 }
 
-// import github from '../../assets/github-mark.png';
-// import React from 'react';
 
-// export default function ProjectCard(props) {
-//   return props.projects.map((project, index) => (
-//     <div key={index} className='project-container'>
-//       <a href={project.url} target="_blank" className="rel p-2">
-//         <div className="project-img-container">
-//           <img src={project.img} alt="" className="project-img" />
-//           <div className="project-info 100w 100h justify-content-center align-items-center my-3">
-//             <h3>{project.name}</h3>
-//             <a className="p-2" href={project.github} target="_blank">
-//               <img className="github-logo" src={github} alt="GitHub" />
-//             </a>
-//           </div>
-//         </div>
-//       </a>
-//     </div>
-//   ));
-// }
 
   
   
